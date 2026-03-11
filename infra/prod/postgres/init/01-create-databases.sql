04e123fb38edbbf54a591691f53fb6c3c00cf01e
Глеб Безруков <g.bezrukov@reo.ru>
2026-03-11 16:54:24 +0600

    fix: stabilize prod matrix bridge deploy

--- infra/prod/postgres/init/01-create-databases.sql
+++ infra/prod/postgres/init/01-create-databases.sql
@@ -1,8 +1,3 @@
-﻿CREATE USER superchat_app WITH PASSWORD ''change-me'';
-CREATE DATABASE superchat_app OWNER superchat_app;
-
-CREATE USER synapse WITH PASSWORD ''change-me'';
-CREATE DATABASE synapse OWNER synapse;
-
-CREATE USER mautrix_telegram WITH PASSWORD ''change-me'';
-CREATE DATABASE mautrix_telegram OWNER mautrix_telegram;
+CREATE DATABASE superchat_app;
+CREATE DATABASE synapse;
+CREATE DATABASE mautrix_telegram;
--- infra/prod/scripts/deploy.sh
+++ infra/prod/scripts/deploy.sh
@@ -5,4 +5,5 @@ ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 ENV_FILE="${1:-$ROOT_DIR/.env}"
 
 "$ROOT_DIR/scripts/preflight.sh" "$ENV_FILE"
+"$ROOT_DIR/scripts/render-configs.sh" "$ENV_FILE"
 docker compose --env-file "$ENV_FILE" -f "$ROOT_DIR/docker-compose.yml" up -d --build
